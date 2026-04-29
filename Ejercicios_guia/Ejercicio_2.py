ea5b0d5aed96e343a432596e70202d1be8764f2d
Luciano Gomez <luciano.gomezv01@gmail.com>
2026-04-29 17:00:22 -0400

    ejercicio 2 listo

--- Ejercicios_guia/Ejercicio_2.py
+++ Ejercicios_guia/Ejercicio_2.py
@@ -1,1 +1,8 @@
-contraseña = 
+contraseña = input("Ingrese su contraseña: ")
+
+if len(contraseña) >= 8 and not contraseña.isalpha() and not contraseña.isnumeric() and contraseña.find(" ") == -1:
+    print("Contraseña fuerte")
+elif len(contraseña) < 8:
+    print("Contraseña muy corta")
+else:
+    print("Error de formato")
