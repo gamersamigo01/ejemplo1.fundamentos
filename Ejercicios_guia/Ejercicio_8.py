17e4ed35cca1adf56bc655a057bafcfc21394eec
Luciano Gomez <luciano.gomezv01@gmail.com>
2026-04-29 22:12:36 -0400

    ejercicio 8 con comentario

--- Ejercicios_guia/Ejercicio_8.py
+++ Ejercicios_guia/Ejercicio_8.py
@@ -1,9 +1,9 @@
 comentario = input("Ingrese su comentario: ")
 
-if comentario.strip() == "":
+if comentario.strip() == "": 
     print("error: ingrese alguna palabra")
-elif "tonto" in comentario.lower() or "feo" in comentario.lower():
-    limpio = comentario.replace("tonto", "*****").replace("feo","***")
+elif "tonto" in comentario.lower() or "feo" in comentario.lower(): #Aca ocupas la funcion lower, asi el comentario que de el usuario no se transforma todo en lower, a diferencia de que si lo ponemos en el input
+    limpio = comentario.replace("tonto", "*****").replace("feo","***") #Se ocupan 2 replaces diferentes para que funcione, pero pueden ser uno seguido de otro
     print(f"Su comentario es {limpio}")
 else:
     print("Error de syntaxis")
