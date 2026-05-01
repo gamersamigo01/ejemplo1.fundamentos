73912c2c81b1e82f3f88c2d7ac4723bbd8eaf7c2
Luciano Gomez <luciano.gomezv01@gmail.com>
2026-04-30 20:38:33 -0400

    Ejercicio 14, Imprime el dia dependiendo de la opcion que ingrese el usuario

---
+++ EJercicios_para_prueba/Ejercicio_14.py
@@ -0,0 +1,18 @@
+opt = int(input("Ingrese una opcion del 1 al 7: "))
+
+if opt == 1:
+    print("Lunes")
+elif opt == 2:
+    print("Martes")
+elif opt == 3:
+    print("Miercoles")
+elif opt == 4:
+    print("Jueves")
+elif opt == 5:
+   print("Viernes")
+elif opt == 6:
+   print("Sabado")
+elif opt == 7:
+   print("Domingo")
+else:
+    print("Error fuera del rango")
