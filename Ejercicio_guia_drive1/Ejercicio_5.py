f86721b7dc894a8087e202d8cc270a403e2b3674
Luciano Gomez <luciano.gomezv01@gmail.com>
2026-05-06 10:20:12 -0400

    Ejercicio (5-6)

---
+++ Ejercicio_guia_drive1/Ejercicio_5.py
@@ -0,0 +1,6 @@
+num = int(input("Ingrese un numero entero: "))
+
+if num % 2 == 0:
+    print(f"Su numero {num} es par")
+else:
+    print(f"Su numero {num} es impar")
---
+++ Ejercicio_guia_drive1/Ejercicio_6.py
@@ -0,0 +1,9 @@
+num1 = int(input("Ingrese su primer numero: "))
+num2 = int(input("Ingrese su segundo numero: "))
+
+if num1 > num2:
+    print(f"{num1} es mayor que {num2}")
+elif num1 < num2:
+    print(f"{num1} es menor que {num2}")
+else:
+    print(f"{num1} es igual que {num2}")
